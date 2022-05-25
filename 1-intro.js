c0a79ea7fa3d8c03c7236e2a0358f98fa4d34859
Azdashir-Al-abboud <azdashiraboud@gmail.com>
2022-05-26 00:22:18 +0300

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,7 @@
+const i = 10;
+
+if (i < 10) {
+    console.log('hello');
+} else {
+    console.log('welcome');
+}
---
+++ 6-alternative-flavor.js
@@ -0,0 +1,12 @@
+module.exports.items = ['item1', 'item2']
+
+const person = {
+    name: 'bob'
+}
+
+module.exports.singlePerson = person
+
+/* or write:
+module.exports.person = {
+    name: 'bob'
+} */
